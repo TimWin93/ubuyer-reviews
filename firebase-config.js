2e78e2b5e4530606eadeeb9c55e64c16f2ac1bdc
TimWin93 <timurkarimov025@gmail.com>
2026-05-12 11:49:37 +0300

    Initial commit — UBuyer review app

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+const FIREBASE_CONFIG = {
+  apiKey:            "AIzaSyC6UB2ABOfITkemR2N2XzHC5hHc13NosQU",
+  authDomain:        "ubuyer-site.firebaseapp.com",
+  projectId:         "ubuyer-site",
+  storageBucket:     "ubuyer-site.firebasestorage.app",
+  messagingSenderId: "65847631209",
+  appId:             "1:65847631209:web:1c10fbca5e6b06bc52f1f8"
+};
